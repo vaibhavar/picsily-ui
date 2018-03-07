363720125d98179769238a1b0b6ab786e895e97b
vaibhavar <webdevpls@gmail.com>
2018-03-08 00:57:19 +0530

    Added GZIP for routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 // Create server instance
 const app = express();
+
+// Enable GZIP
+app.use(compression());
 
 // Serve UI files from dist/ directory
 app.use(express.static('./dist/'));
